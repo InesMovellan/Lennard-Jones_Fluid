fb7c939a5b29e910816e2c8f5250f7bfac84c307
InesMovellan <sanchezdemovellan.ines@gmail.com>
2020-11-04 19:34:50 +0100

    Build a xyz file with the intial geometry, compute the inter-particle distances and store them

--- LJfluid.f90
+++ LJfluid.f90
@@ -10,35 +10,86 @@
 module LJfluid
 
     implicit none
-    public :: initial_geom
+    public :: initial_geom, energy
     contains
 
-    ! ***********************************************************************************************
+    ! *********************************************************************************************
     !                                SUBROUTINE 1: Set the initial geometry
     
-    subroutine initial_geom(n,L,pos)
+    subroutine initial_geom(n,L,geom0)
     
         ! Declaration statements
-        integer, intent(in) :: n, L   
-        real(kind=8), allocatable, intent(inout) :: pos(:,:)
-        character(20) :: pos_format
+        integer :: i, j
+        integer, intent(in) :: n, L  ! Number of particles (n) and lenght of the cubic box (L)
+        real(kind=8), allocatable, intent(inout) :: geom0(:,:)
+        real(kind=8), allocatable :: xyz(:)
 
         ! Execution zone
-        allocate(pos(n,n))
-        write(pos_format, '(a, i0, a)') '(', n, 'f10.6)'
-        call random_number(pos)
-        pos = L*pos
-        write(*,pos_format) pos
+        ! The initial coordinates of the particles are stored in a matrix called pos0, which
+        ! has the following structure:
+        ! x1  y1  z1
+        ! x2  y2  z2
+        ! x3  y3  z3
+        !     :
+        ! xn  yn  zn
+        allocate(geom0(3,n))
+        call random_number(geom0)
+        geom0(1,1) = 1.
+        geom0(2,1) = 0.
+        geom0(3,1) = 0.
+        geom0(1,2) = 1.
+        geom0(2,2) = 1.
+        geom0(3,2) = 1.
+        geom0(1,3) = 1.
+        geom0(2,3) = 3.
+        geom0(3,3) = 3.
 
+        geom0 = L*geom0
+        ! The initial geometry is written in a .xyz file, which can be read with 3D visualization
+        ! programs (vesta, vmd, avogadro). In order to do that we use a auxiliar vector xyz
+        open(23, file="initial_geom.xyz", action="write")
+        write(23,'(I4)') n
+        write(23,*) " "
+        allocate(xyz(3))
+        do i = 1, n
+            xyz = geom0(:,i)
+            write(23,'( "H", f10.6, f10.6, f10.6)' ) xyz(1), xyz(2), xyz(3)
+        enddo
         return
 
     end subroutine initial_geom
 
-    ! ***********************************************************************************************
+    ! *********************************************************************************************
 
-    ! ***********************************************************************************************
+    ! *********************************************************************************************
     !                                SUBROUTINE 2: Compute the energy
     
+    subroutine energy(n, geom0, r12, r)
 
-    ! ***********************************************************************************************
+        ! Declaration statements
+        integer :: i,j
+        integer, intent(in) :: n
+        real(kind=8), dimension(3,n), intent(in) :: geom0
+        real(kind=8), allocatable, intent(inout) :: r(:,:)
+        real(kind=8) :: r12
+
+        ! Execution zone
+        allocate(r(n-1,n-1))
+        r = 0.d0
+        do i = 2, n
+            do j = 1, i-1
+                r(i-1,j) = (geom0(1,i)-geom0(1,j))**2+(geom0(2,i)-geom0(2,j))**2+(geom0(3,i)-geom0(3,j))**2
+                write(*,*) r(i-1,j)
+
+            enddo
+        enddo
+        write(*,*) " "
+        write(*,'(2f10.6)') r
+        r12 = (geom0(1,2)-geom0(1,1))**2+(geom0(2,2)- geom0(2,1))**2+(geom0(3,2)- geom0(3,1))**2
+        !write(*,*) r12
+        !write(*,*) r
+
+    end subroutine energy
+
+    ! *********************************************************************************************
 end module LJfluid
--- main.f90
+++ main.f90
@@ -11,27 +11,17 @@ program main
 
     implicit none
     integer :: n, L
-    real(kind=8), allocatable :: x(:,:)
-    ! real(kind=8), allocatable :: A(:,:)               
-    ! real(kind=8) :: R2                                
-    ! character(50) :: Aformat, cformat                 
+    real(kind=8), allocatable :: coord(:,:)
+    real(kind=8), allocatable :: dist(:)
+    real(kind=8) :: r
 
-    ! ----------------------------------------------------------------------------------------------
+    ! --------------------------------------------------------------------------------------------
 
     ! Execution zone
-    n = 10
-    L = 10
-    call initial_geom(n,L,x)
-  
-    ! We open the out.dat file, in which we will write the results
-    !open(23, file="out.dat", action="write")
-    ! The title of the output is written
-    !write(23,*) " "
-    
-    ! The subroutine read_data is called, which reads the values of points (x,y) from input.dat file
-    !call read_data(n,x,y)
-    !do i = 1, n
-    !    write(23,'(2f24.16)') x(i), y(i)
-    !enddo
+    n = 3
+    L = 1
+    call initial_geom(n,L,coord)
+
+    call energy(n,coord,r,dist)
 
 end program main
